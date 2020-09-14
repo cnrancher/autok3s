74fb1bf0d929a2e149f086e391f0115d04893b95
Jason-ZW <zhenyang@rancher.com>
2020-09-14 13:58:49 +0800

    fix(alibaba): remove duplicate instance ids
    Signed-off-by: Jason-ZW <zhenyang@rancher.com>

--- pkg/utils/util.go
+++ pkg/utils/util.go
@@ -13,3 +13,20 @@ func RandomToken(size int) (string, error) {
 	}
 	return hex.EncodeToString(token), err
 }
+
+func UniqueArray(origin []string) (unique []string) {
+	unique = make([]string, 0)
+	for i := 0; i < len(origin); i++ {
+		repeat := false
+		for j := i + 1; j < len(origin); j++ {
+			if origin[i] == origin[j] {
+				repeat = true
+				break
+			}
+		}
+		if !repeat {
+			unique = append(unique, origin[i])
+		}
+	}
+	return
+}
