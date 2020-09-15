af4b4367e04f0702407fa6ed1c2ec966f3ccae0b
Siye Wang <siye@rancher.com>
2020-09-15 14:28:01 +0800

    feat(autok3s): support delete command
    Signed-off-by: Siye Wang <siye@rancher.com>

---
+++ cmd/delete.go
@@ -0,0 +1,64 @@
+package cmd
+
+import (
+	"github.com/cnrancher/autok3s/cmd/common"
+	"github.com/cnrancher/autok3s/pkg/providers"
+
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+var (
+	deleteCmd = &cobra.Command{
+		Use:     "delete",
+		Short:   "Delete k3s cluster",
+		Example: `  autok3s delete --name cluster`,
+	}
+	dProvider = ""
+	force    = false
+	dp        providers.Provider
+)
+
+func init() {
+	deleteCmd.Flags().StringVarP(&dProvider, "provider", "p", dProvider, "Provider is a module which provides an interface for managing cloud resources")
+	deleteCmd.Flags().BoolVarP(&force, "force", "f", force, "Force delete cluster")
+}
+
+func DeleteCommand() *cobra.Command {
+
+	pStr := common.FlagHackLookup("--provider")
+
+	if pStr != "" {
+		if reg, err := providers.Register(pStr); err != nil {
+			logrus.Fatalln(err)
+		} else {
+			dp = reg
+		}
+
+		deleteCmd.Flags().AddFlagSet(dp.GetCredentialFlags(deleteCmd))
+		deleteCmd.Flags().AddFlagSet(dp.GetDeleteFlags(deleteCmd))
+	}
+
+	deleteCmd.Run = func(cmd *cobra.Command, args []string) {
+		common.BindPFlags(cmd, dp)
+
+		// read options from config.
+		if err := viper.ReadInConfig(); err != nil {
+			logrus.Fatalln(err)
+		}
+
+		// sync config data to local cfg path.
+		if err := viper.WriteConfig(); err != nil {
+			logrus.Fatalln(err)
+		}
+
+		dp.GenerateClusterName()
+
+		if err := dp.DeleteK3sNode(force); err != nil {
+			logrus.Fatalln(err)
+		}
+	}
+
+	return deleteCmd
+}
