8d6e52d2544017eb4d4463a7947a061d340cc7a5
Jason-ZW <zhenyang@rancher.com>
2021-03-18 10:43:15 +0800

    fix(autok3s): fix pprof url path error

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -44,16 +44,18 @@ func Start() http.Handler {
 	})
 
 	// profiling handlers for pprof under /debug/pprof
-	router.HandleFunc("/debug/pprof", pprof.Index)
+	router.HandleFunc("/debug/pprof/", pprof.Index)
 	router.HandleFunc("/debug/pprof/trace", pprof.Trace)
 	router.HandleFunc("/debug/pprof/profile", pprof.Profile)
 	router.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+	router.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
 
 	// Manually add support for paths linked to by index page at /debug/pprof/
 	router.Handle("/debug/pprof/goroutine", pprof.Handler("goroutine"))
 	router.Handle("/debug/pprof/heap", pprof.Handler("heap"))
 	router.Handle("/debug/pprof/threadcreate", pprof.Handler("threadcreate"))
 	router.Handle("/debug/pprof/block", pprof.Handler("block"))
+	router.Handle("/debug/pprof/mutex", pprof.Handler("mutex"))
 
 	router.Path("/{prefix}/{type}").Handler(s)
 	router.Path("/{prefix}/{type}/{name}").Queries("link", "{link}").Handler(s)
