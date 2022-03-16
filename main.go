8eef053901af475ff1da74daa2f355b35b6f3191
Jason-ZW <zhenyang@rancher.com>
2022-03-16 16:33:20 +0800

    fix(metrics): monitor API requests
    Signed-off-by: Jason-ZW <zhenyang@rancher.com>

--- main.go
+++ main.go
@@ -1,19 +1,15 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
 	"math/rand"
-	"net/http"
 	"os"
 	"path/filepath"
 	"time"
 
 	"github.com/cnrancher/autok3s/cmd"
 	"github.com/cnrancher/autok3s/pkg/cli/kubectl"
-	"github.com/cnrancher/autok3s/pkg/common"
+	"github.com/cnrancher/autok3s/pkg/metrics"
 
-	"github.com/Jason-ZW/autok3s-geo/pkg/types"
 	"github.com/docker/docker/pkg/reexec"
 	"github.com/spf13/cobra"
 )
@@ -48,45 +44,10 @@ func main() {
 		cmd.SSHCommand(), cmd.DescribeCommand(), cmd.ServeCommand(), cmd.ExplorerCommand())
 
 	rootCmd.PersistentPreRun = func(cmd *cobra.Command, args []string) {
-		ReportMetrics()
+		metrics.ReportMetrics()
 	}
 
 	if err := rootCmd.Execute(); err != nil {
 		os.Exit(1)
-	}
-}
-
-func ReportMetrics() {
-	logger := common.NewLogger(common.Debug, nil)
-
-	client := &http.Client{}
-
-	b, err := json.Marshal(types.GeoIP{})
-	if err != nil {
-		logger.Debugf("failed to collected usage metrics: %s", err.Error())
-		return
-	}
-
-	req, err := http.NewRequest("POST", metricsEndpoint, bytes.NewBuffer(b))
-	if err != nil {
-		logger.Debugf("failed to collected usage metrics: %s", err.Error())
-		return
-	}
-
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Req-Source", metricsSourceTag)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		logger.Debugf("failed to collected usage metrics: %s", err.Error())
-		return
-	}
-
-	defer func() {
-		_ = resp.Body.Close()
-	}()
-
-	if resp.StatusCode != http.StatusOK && resp.StatusCode != http.StatusCreated {
-		logger.Debugf("failed to collected usage metrics: %s", resp.Status)
 	}
 }
