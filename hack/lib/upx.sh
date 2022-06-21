4f2cea4566e7e9263050d13648c0348ee5fd0728
Yuxing Deng <jxfa0043379@hotmail.com>
2022-06-21 15:25:00 +0800

    fix(ci): Set compress rate to 5
    For faster build speed

--- hack/lib/upx.sh
+++ hack/lib/upx.sh
@@ -19,8 +19,10 @@ function autok3s::upx::run() {
       autok3s::log::info "darwin arm64 binary doesn't work well with upx, skipping compressing this binary."
       continue
     fi
+    autok3s::log::info "compressing $file"
     # Fix issue: https://github.com/cnrancher/autok3s/issues/456.
-    upx -9 ${ROOT_DIR}/dist/$file;
+    upx -5 ${ROOT_DIR}/dist/$file;
+    autok3s::log::info "compress $file done"
   done
 }
 
