798896bf955a44b075ec4586f52483888134b385
Yuxing Deng <jxfa0043379@hotmail.com>
2022-12-27 16:58:33 +0800

    feat(autpk3s): Drop harvester provider

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,7 +13,6 @@ import (
 	_ "github.com/cnrancher/autok3s/pkg/providers/alibaba"
 	_ "github.com/cnrancher/autok3s/pkg/providers/aws"
 	_ "github.com/cnrancher/autok3s/pkg/providers/google"
-	_ "github.com/cnrancher/autok3s/pkg/providers/harvester"
 	_ "github.com/cnrancher/autok3s/pkg/providers/k3d"
 	_ "github.com/cnrancher/autok3s/pkg/providers/native"
 	_ "github.com/cnrancher/autok3s/pkg/providers/tencent"
